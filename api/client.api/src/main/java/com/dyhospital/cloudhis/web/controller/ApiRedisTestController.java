0c2e987eb4804a92cbf9ec64816348a82ca7dade
Sunshine <32347159+zjhzzhouzhou@users.noreply.github.com>
2019-06-18 11:16:47 +0800

    Update ApiRedisTestController.java
    错误提交

--- api/client.api/src/main/java/com/dyhospital/cloudhis/web/controller/ApiRedisTestController.java
+++ api/client.api/src/main/java/com/dyhospital/cloudhis/web/controller/ApiRedisTestController.java
@@ -39,6 +39,7 @@ public class ApiRedisTestController {
     @ApiOperation("缓存查询")
     @RequestMapping(value = MessageApi.REDIS_GET_STR, method = RequestMethod.GET)
     public String getStrValue(String key) {
+        xxxx = xxx;
         return customerMessageService.getStrValue(key);
     }
 
